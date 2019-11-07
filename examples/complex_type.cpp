e103d5e490748edb88fab2bf0236284b80e03a34
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2019-11-07 14:39:38 +0100

    Added casting operator for DynamicData.

--- examples/complex_type.cpp
+++ examples/complex_type.cpp
@@ -21,24 +21,19 @@ int main()
     outer.add_member("om8", ArrayType(inner, 4));
 
     DynamicData data(outer);
-    data["om1"].value(6.7);                                //PrimitiveType<double>
-    data["om2"]["im1"].value(42u);                         //PrimitiveType<uint32_t>
-    data["om2"]["im2"].value(35.8f);                       //PrimitiveType<float>
-    data["om3"].value<std::string>("Hi!");                 //StringType
-    data["om3"].string("This is a string");                //...
-    data["om4"].wstring(L"Also support unicode! \u263A");  //WStringType
+    data["om1"] = 6.7;                                     //PrimitiveType<double>
+    data["om2"]["im1"] = 42u;                              //PrimitiveType<uint32_t>
+    data["om2"]["im2"] = 35.8f;                            //PrimitiveType<float>
+    data["om3"] = "This is a string";                      //...
+    data["om4"] = L"Also support unicode! \u263A";         //WStringType
     data["om5"].push(12u);                                 //SequenceType(PrimitiveType<uint32_t>)
     data["om5"].push(31u);                                 //...
     data["om5"].push(50u);                                 //...
-    data["om5"][1].value(100u);                            //...
+    data["om5"][1] = 100u;                                 //...
     data["om6"].push(data["om2"]);                         //SequenceType(inner)
     data["om6"][0] = data["om2"];                          //...
-    data["om7"][1].value(123u);                            //ArrayType(PrimitiveType<uint32_t>)
+    data["om7"][1] = 123u;                                 //ArrayType(PrimitiveType<uint32_t>)
     data["om8"][1] = data["om2"];                          //ArrayType(inner)
-
-    data["om1"] = 8.88;                                    //PrimitiveType<double>
-    data["om3"] = "This is a string, assignable too.";     //StringType
-    data["om4"] = L"WStrings attack again! \u263A";        //WStringType
 
     std::cout << data.to_string() << std::endl; //See to_string implementation as an example of data instrospection
 
