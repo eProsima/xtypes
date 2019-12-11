0aa8197ef402f9ba3cd2ffdafe05f26ef7da88ef
Luis Gasco <luisgasco@eprosima.com>
2019-12-11 16:31:19 +0100

    fix

--- test/unitary/main.cpp
+++ test/unitary/main.cpp
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-I SHOULDN'T COMPILE LOL
+// LET COMPILE AGAIN
 #include <gtest/gtest.h>
 #include <xtypes/xtypes.hpp>
 #include <xtypes/Module.hpp>
