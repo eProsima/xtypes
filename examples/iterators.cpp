7ef07312f648aebaa5f5f57b122247e67efaa858
Luis Gasco <luisgasco@eprosima.com>
2019-12-13 13:10:46 +0100

    Refs #6980 Added new examples. (#43)
    * Refs #6980 Added new examples.
    
    * Improve modules example.

---
+++ examples/iterators.cpp
@@ -0,0 +1,263 @@
+#include <xtypes/xtypes.hpp>
+#include <xtypes/Module.hpp>
+
+#include <iostream>
+
+using namespace eprosima::xtypes;
+
+int main()
+{
+    std::cout << "--- Iterate string ---" << std::endl;
+    {
+        StringType string;
+        DynamicData str1(string);
+
+        str1.value<std::string>("Hello!");
+        for (ReadableDynamicDataRef&& elem : str1)
+        {
+            std::cout << elem.value<char>();
+        }
+        std::cout << std::endl;
+
+        // Hello! -> Ifmmp"
+        for (WritableDynamicDataRef&& elem : str1)
+        {
+            elem = static_cast<char>(elem.value<char>() + 1);
+        }
+
+        for (ReadableDynamicDataRef&& elem : str1)
+        {
+            std::cout << elem.value<char>();
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "--- Iterate array ---" << std::endl;
+    {
+        ArrayType array_type(primitive_type<int32_t>(), 10);
+        DynamicData array(array_type);
+
+        for (int i = 0; i < 10; ++i)
+        {
+            array[i] = 5 * i;
+        }
+
+        int32_t check_sum = 0;
+        for (ReadableDynamicDataRef&& elem : array)
+        {
+            check_sum += elem.value<int32_t>();
+        }
+
+        for (WritableDynamicDataRef&& elem : array)
+        {
+            elem = elem.value<int32_t>() * 2;
+        }
+
+        int32_t double_check_sum = 0;
+        for (ReadableDynamicDataRef&& elem : array)
+        {
+            double_check_sum += elem.value<int32_t>();
+        }
+
+        std::cout << "Array Check_sum is: " << check_sum << " and its double is: " << double_check_sum << std::endl;
+    }
+
+    std::cout << "--- Iterate sequence ---" << std::endl;
+    {
+        SequenceType seq_type(primitive_type<int32_t>());
+        DynamicData seq(seq_type);
+
+        for (int i = 0; i < 10; ++i)
+        {
+            seq.push(5 * i);
+        }
+
+        int32_t check_sum = 0;
+        for (ReadableDynamicDataRef&& elem : seq)
+        {
+            check_sum += elem.value<int32_t>();
+        }
+
+        for (WritableDynamicDataRef&& elem : seq)
+        {
+            elem = elem.value<int32_t>() * 2;
+        }
+
+        int32_t double_check_sum = 0;
+        for (ReadableDynamicDataRef&& elem : seq)
+        {
+            double_check_sum += elem.value<int32_t>();
+        }
+        std::cout << "Sequence Check_sum is: " << check_sum << " and its double is: " << double_check_sum << std::endl;
+    }
+
+    std::cout << "--- Iterate struct data (wide) ---" << std::endl;
+    {
+        StructType my_struct("MyStruct");
+        my_struct.add_member("my_int", primitive_type<int32_t>());
+        my_struct.add_member("my_double", primitive_type<double>());
+
+        DynamicData my_data(my_struct);
+        my_data["my_int"] = 55;
+        my_data["my_double"] = -23.44;
+
+        //ReadableDynamicDataRef::MemberIterator it = my_data.citems().begin();
+        auto it = my_data.citems().begin();
+        auto wit = my_data.items().begin();
+
+        for (ReadableDynamicDataRef::MemberPair&& elem : my_data.items())
+        {
+            switch(elem.kind())
+            {
+            case TypeKind::INT_32_TYPE:
+                std::cout << elem.member().name() << ": " << elem.data().value<int32_t>() << std::endl;
+                break;
+            case TypeKind::FLOAT_64_TYPE:
+                std::cout << elem.member().name() << ": " << elem.data().value<double>() << std::endl;
+                break;
+            default:
+                break;
+            }
+        }
+
+        for (WritableDynamicDataRef::MemberPair&& elem : my_data.items())
+        {
+            switch(elem.kind())
+            {
+            case TypeKind::INT_32_TYPE:
+                elem.data() = elem.data().value<int32_t>() * 2;
+                break;
+            case TypeKind::FLOAT_64_TYPE:
+                elem.data() = elem.data().value<double>() * 2;
+                break;
+            default:
+                break;
+            }
+        }
+
+        for (ReadableDynamicDataRef::MemberPair&& elem : my_data.items())
+        {
+            switch(elem.kind())
+            {
+            case TypeKind::INT_32_TYPE:
+                std::cout << elem.member().name() << " * 2: " << elem.data().value<int32_t>() << std::endl;
+                break;
+            case TypeKind::FLOAT_64_TYPE:
+                std::cout << elem.member().name() << " * 2: " << elem.data().value<double>() << std::endl;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    std::cout << "--- Iterate struct type (depth) ---" << std::endl;
+    {
+        StructType l2 = StructType("Level2")
+            .add_member("l2m1", primitive_type<uint32_t>())
+            .add_member("l2m2", primitive_type<float>())
+            .add_member("l2m3", StringType())
+            .add_member("l2m4", WStringType());
+
+        StructType l1 = StructType("Level1")
+            .add_member("l1m1", SequenceType(primitive_type<uint32_t>()))
+            .add_member("l1m2", SequenceType(l2))
+            .add_member("l1m3", ArrayType(primitive_type<uint32_t>(), 2))
+            .add_member("l1m4", ArrayType(l2, 4))
+            .add_member("l1m5", l2);
+
+        StructType l0 = StructType("Level0")
+            .add_member("l0m1", l1)
+            .add_member("l0m2", l2);
+
+        l0.for_each([&](const DynamicType::TypeNode& node)
+        {
+            for (size_t tabs = 0; tabs < node.deep(); ++tabs)
+            {
+                std::cout << "  ";
+            }
+            std::cout << node.type().name();
+            if (node.has_parent() && node.from_member() != nullptr)
+            {
+                std::cout << " " << node.from_member()->name();
+            }
+            std::cout << std::endl;
+        });
+    }
+
+    std::cout << "--- Iterate struct data (depth) ---" << std::endl;
+    {
+        StructType l2 = StructType("Level2")
+            .add_member("l2m1", primitive_type<uint32_t>())
+            .add_member("l2m2", primitive_type<float>())
+            .add_member("l2m3", StringType());
+
+        StructType l1 = StructType("Level1")
+            .add_member("l1m1", SequenceType(primitive_type<uint32_t>()))
+            .add_member("l1m2", SequenceType(l2))
+            .add_member("l1m3", ArrayType(primitive_type<uint32_t>(), 2))
+            .add_member("l1m4", ArrayType(l2, 4))
+            .add_member("l1m5", l2);
+
+        StructType l0 = StructType("Level0")
+            .add_member("l0m1", l1)
+            .add_member("l0m2", l2);
+
+        uint32_t uint_value = 0;
+        DynamicData data(l0);
+        data["l0m1"]["l1m1"].push(uint_value++);    // [0] = 0
+        data["l0m1"]["l1m1"].push(uint_value++);    // [1] = 1
+        data["l0m1"]["l1m1"].push(uint_value++);    // [2] = 2
+        {
+            DynamicData l2data(l2);
+            l2data["l2m1"] = uint_value++;          // 3
+            l2data["l2m2"] = 12.345f;
+            l2data["l2m3"] = "l2m3";
+            data["l0m1"]["l1m2"].push(l2data);      // [0]
+            data["l0m1"]["l1m2"].push(l2data);      // [1]
+        }
+        data["l0m1"]["l1m3"][0] = uint_value++;     // 4
+        data["l0m1"]["l1m3"][1] = uint_value++;     // 5
+        {
+            DynamicData l2data(l2);
+            l2data["l2m1"] = uint_value++;          // 6
+            l2data["l2m2"] = 123.45f;
+            l2data["l2m3"] = "l2m3_bis";
+            data["l0m1"]["l1m4"][0] = l2data;       // [0]
+            data["l0m1"]["l1m4"][1] = l2data;       // [1]
+            data["l0m1"]["l1m4"][2] = l2data;       // [2]
+            data["l0m1"]["l1m4"][3] = l2data;       // [3]
+        }
+        {
+            DynamicData l2data(l2);
+            l2data["l2m1"] = uint_value++;          // 7
+            l2data["l2m2"] = 1234.5f;
+            l2data["l2m3"] = "l2m3_bis_2";
+            data["l0m1"]["l1m5"] = l2data;
+        }
+        data["l0m2"]["l2m1"] = uint_value++;        // 8
+        data["l0m2"]["l2m2"] = 12345.f;
+        data["l0m2"]["l2m3"] = "l2m3_bis_3";
+
+
+        data.for_each([&](const DynamicData::ReadableNode& node)
+        {
+            if (!node.has_parent()) return; // Ignore the root
+            for (size_t tabs = 0; tabs < node.deep(); ++tabs)
+            {
+                std::cout << "  ";
+            }
+            if (node.from_member() != nullptr)
+            {
+                std::cout << node.from_member()->name();
+                if (node.type().is_primitive_type() || node.type().kind() == TypeKind::STRING_TYPE)
+                {
+                    std::cout << ": " << node.data().cast<std::string>();
+                }
+                std::cout << std::endl;
+            }
+        });
+    }
+
+    return 0;
+}
---
+++ examples/module.cpp
@@ -0,0 +1,57 @@
+#include <xtypes/xtypes.hpp>
+#include <xtypes/Module.hpp>
+
+#include <iostream>
+
+using namespace eprosima::xtypes;
+
+int main()
+{
+    std::string idl_spec = R"(
+        struct InnerType
+        {
+            uint32 im1;
+            float im2;
+        };
+    )";
+
+    idl::Context context = idl::parse(idl_spec);
+    const StructType& inner = context.module().structure("InnerType");
+
+    StructType outer("OuterType");
+    outer.add_member("om1", StringType());
+
+    Module root;
+    Module& submod_a = root.create_submodule("a");
+    Module& submod_b = root.create_submodule("b");
+    Module& submod_aa = submod_a.create_submodule("a");
+    submod_aa.structure(outer);
+    submod_b.structure(inner);
+
+    /*
+    root
+        \_a
+           \_ a _ OuterType
+
+        \_b _ InnerType
+    */
+
+    std::cout << std::boolalpha;
+    std::cout << "Does a::a::OuterType exists?: " << root.has_structure("a::a::OuterType") << std::endl;
+    std::cout << "Does ::InnerType exists?: " << root.has_structure("::InnerType") << std::endl;
+    std::cout << "Does InnerType exists?: " << root.has_structure("InnerType") << std::endl;
+    std::cout << "Does OuterType exists?: " << root.has_structure("OuterType") << std::endl;
+    std::cout << "Does ::b::InnerType exists?: " << root.has_structure("::b::InnerType") << std::endl;
+    std::cout << "Does b::InnerType exists?: " << root.has_structure("b::InnerType") << std::endl;
+
+    DynamicData outer_data(root["a"]["a"].structure("OuterType")); // ::a::a::OuterType
+    outer_data["om1"] = "This is a string.";
+
+    std::string scope_b = submod_b.scope();
+    std::string scope_inner_type = scope_b + "::" + inner.name(); // b::InnerType
+    DynamicData inner_data(root.structure(scope_inner_type));
+    inner_data["im1"] = 32u;
+    inner_data["im2"] = 3.14159265f;
+
+    return 0;
+}
